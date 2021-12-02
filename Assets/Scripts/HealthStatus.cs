059d72d8be56e1cf6611253d0e20f1a9f8d62c3c
dabyko <danylobykon@gmail.com>
2021-12-02 19:56:21 +0200

    add linear progress bar

---
+++ Assets/Scripts/HealthStatus.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[ExecuteInEditMode()]
+public class HealthStatus : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [MenuItem("GameObject/UI/LinearHealthProgressBar")]
+    public static void AddHealthProgressBar()
+    {
+        GameObject obj = Instantiate(Resources.Load<GameObject>("UI/LinearHealthProgressBar"));
+        obj.transform.SetParent(Selection.activeGameObject.transform, false);
+    }
+#endif
+    public int minimum;
+
+    public int maximum;
+
+    public int current;
+
+    public Image mask;
+
+    public Color fillColor;
+
+    public Image fill;
+
+   
+    private void Update()
+    {
+        CheckFill();
+    }
+
+    void CheckFill()
+    {
+        float currentOffset = current - minimum;
+        float maximumOffset = maximum - minimum;
+        float fillAmount = currentOffset / maximumOffset;
+
+        mask.fillAmount = fillAmount;
+
+        fill.color = fillColor;
+    }
+}
